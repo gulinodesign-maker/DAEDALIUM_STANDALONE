bbbc15443ea39e1755d6711d03f9cf8a17d55df7
gulinodesign-maker <gulinodesign@gmail.com>
2026-03-06 17:49:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ try{
 /* global API_BASE_URL, API_KEY */
 
 /**
- * Build: 2.082
+ * Build: 2.083
  */
-const BUILD_VERSION = "2.082";
+const BUILD_VERSION = "2.083";
 
 // Local DB keys (local-first)
 const __DB_KEYS__ = {
@@ -15032,11 +15032,6 @@ function toRoman(n){
 }
 
 
-(async ()=>{ try{ await init(); } catch(e){ console.error(e); try{ toast(e.message||"Errore"); }catch(_){ } } })();
-
-
-
-
 /* =========================
    Lavanderia (dDAE_1.020)
 ========================= */
@@ -16226,3 +16221,5 @@ async function initOrePuliziaPage(){
 
   __renderOrePuliziaCalendar_();
 }
+
+(async ()=>{ try{ await init(); } catch(e){ console.error(e); try{ toast(e.message||"Errore"); }catch(_){ } } })();
