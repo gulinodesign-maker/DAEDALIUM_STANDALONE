88802df086dc56776f0047f11b769d72f03e2ba8
gulinodesign-maker <gulinodesign@gmail.com>
2026-03-10 18:46:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ try{
 /* global API_BASE_URL, API_KEY */
 
 /**
- * Build: 2.140
+ * Build: 2.141
  */
-const BUILD_VERSION = "2.140";
+const BUILD_VERSION = "2.141";
 
 // Local DB keys (local-first)
 const __DB_KEYS__ = {
