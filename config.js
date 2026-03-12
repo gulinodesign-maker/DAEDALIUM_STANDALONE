7a0e7f229d988a3ef1433eaf38f8eb6e75db0275
gulinodesign-maker <gulinodesign@gmail.com>
2026-03-12 21:54:05 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
-/* dDAE_2.168 - Firebase frontend config */
-const FIREBASE_ENABLED = true;
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDYG-TqrzfN5kDUH9GYWmYbqnwoqEnWMkM",
-  authDomain: "ddae-sync.firebaseapp.com",
-  projectId: "ddae-sync",
-  storageBucket: "ddae-sync.firebasestorage.app",
-  messagingSenderId: "460884105746",
-  appId: "1:460884105746:web:29e06fc11d43c78bdd96cb",
-  measurementId: "G-Q2BXTMGR96"
+window.API_BASE_URL = window.API_BASE_URL || "INCOLLA_QUI_API_BASE_URL";
+window.API_KEY = window.API_KEY || "daedalium2026";
+window.FIREBASE_ENABLED = (typeof window.FIREBASE_ENABLED === "boolean") ? window.FIREBASE_ENABLED : false;
+window.FIREBASE_CONFIG = window.FIREBASE_CONFIG || {
+  apiKey: "",
+  projectId: ""
 };
