dbcf58293a15bbcbb59dbb52ca2d3fc30ef2fdf8
gulinodesign-maker <gulinodesign@gmail.com>
2026-04-17 22:16:07 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* dDAE - Service Worker (PWA)
- * Build: 2.664
+ * Build: 2.665
  *
  * Obiettivi:
  * - cache name cambia ad ogni build
@@ -9,7 +9,7 @@
  * - fix iOS/Safari cache aggressiva (cache:"reload"/"no-store" + query ?v)
  */
 
-const BUILD = "2.664";
+const BUILD = "2.665";
 const CACHE_NAME = `dDAE-local-cache-${BUILD}`; // cambia ad ogni build // cambia ad ogni build
 
 // Asset principali (versionati per forzare il fetch anche con cache aggressiva iOS)
