188062755ba3ec8b0f3008438306a90669ea5516
gulinodesign-maker <gulinodesign@gmail.com>
2026-06-09 17:53:04 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/* dDAE_3.018 - Firebase frontend config */
+/* dDAE_3.019 - Firebase frontend config */
 const DDAE_LICENSE_PAYMENT_URL = "";
 const FIREBASE_ENABLED = true;
 const FIREBASE_CONFIG = {
