f67ac2a6a446cbcf82334f41ebdba2aa6228ac78
gulinodesign-maker <gulinodesign@gmail.com>
2026-09-11 17:21:31 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/* dDAE_3.309 - Firebase frontend config + traduzione al salvataggio + endpoint opzionale */
+/* dDAE_3.311 - Firebase frontend config + traduzione al salvataggio + endpoint opzionale */
 const DDAE_LICENSE_PAYMENT_URL = "";
 
 // Endpoint di traduzione opzionale usato solo come fallback durante il salvataggio delle traduzioni.
